d9fee9d7a4916f6736ff2f9cc6a9ec7473985514
airmenkabza <157689162+skywalkerkabza@users.noreply.github.com>
2024-04-11 10:24:38 +0200

    Kabelo Modipa
    Home work

---
+++ Javascript/MyWork/8_ArrayManipulation.js
@@ -0,0 +1,5 @@
+const numbers = [25, 10, 35, 18, 42, 5];
+const maxNumber = Math.max(...numbers);
+const minNumber = Math.min(...numbers);
+console.log("Maximum Number:", maxNumber);
+console.log("Minimum Number:", minNumber);
---
+++ Javascript/MyWork/9_StringManipulation.js
@@ -0,0 +1,7 @@
+const sentence = "This is a sample sentence.";
+const wordCount = sentence.split(" ").length;
+console.log("Word Count:", wordCount);
+for(let ch of sentence)
+{
+    console.log(ch);
+}
